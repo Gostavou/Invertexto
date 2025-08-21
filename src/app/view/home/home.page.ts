428f0b8e67d29ff77ec8ed7378ea946bcd39e369
Gostavou <zeupitbul@hotmail.com>
2025-08-20 21:02:05 -0300

    first commit

--- src/app/view/home/home.page.ts
+++ src/app/view/home/home.page.ts
@@ -2,28 +2,25 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { people, pin } from 'ionicons/icons'
-
-addIcons({
-  people: people,
-  pin: pin
-})
+import { people, pin } from 'ionicons/icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon], 
+  standalone: true,
+  imports: [IonIcon, IonButton, IonHeader, IonToolbar, IonTitle, IonContent],
 })
-
 export class HomePage {
-  constructor(private router: Router) {}
-
-  goToPessoaPage(){
+  constructor(private router: Router) {
+    addIcons({ people, pin });
+  }
+  
+  goToPessoaPage() {
     this.router.navigate(['pessoa']);
   }
-
-  goToCepPage(){
+  
+  goToCepPage() {
     this.router.navigate(['cep']);
   }
 }
